3fbbbf691a63aaf338e92e82594d720ea37afcff
Elliot Waddington <Elliot.Waddington@havas.com>
2017-12-09 14:51:33 +0000

    adding rest of Football league teams select club now in tabs per league adding ability to run in simulator styling tweaks to header/colours

--- src/pages/ClubDetails/ClubDetails.ts
+++ src/pages/ClubDetails/ClubDetails.ts
@@ -1,6 +1,12 @@
-import { Component } from '@angular/core';
+import {
+  Component
+} from '@angular/core';
 
-import { NavController, NavParams } from 'ionic-angular';
+import {
+  NavController,
+  NavParams,
+  AlertController
+} from 'ionic-angular';
 
 @Component({
   selector: 'ClubDetails',
@@ -8,13 +14,29 @@ import { NavController, NavParams } from 'ionic-angular';
 })
 export class ClubDetails {
   selectedClub: any;
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public alertCtrl: AlertController) {
     // If we navigated to this page, we will have a club available as a nav param
     this.selectedClub = navParams.get('club');
   }
   selectClub($event) {
     const clubStadiumImage = document.querySelector('.club-stadium');
+    const selectedClub = document.querySelector('#club').textContent;
     const selectClubButton = $event.target;
+    setTimeout(() => {
+      let alert = this.alertCtrl.create({
+        title: 'Congratulations!',
+        subTitle: `You have selected ${selectedClub} as your team!`,
+        buttons: [{
+          text: 'Signup!',
+          handler: data => {
+            console.log('Go to sign up');
+          }
+        }]
+      });
+      alert.present();
+
+    }, 800);
+
     selectClubButton.disabled = true;
     clubStadiumImage.classList.add('activated');
   }
